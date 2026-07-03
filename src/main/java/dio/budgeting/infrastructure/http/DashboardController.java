ef78a7ccbaa087cf1bc9a47339e40630ae434639
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2026-07-02 21:04:33 -0400

    fix: create constructor to controller dashboard

--- src/main/java/dio/budgeting/infrastructure/http/DashboardController.java
+++ src/main/java/dio/budgeting/infrastructure/http/DashboardController.java
@@ -1,5 +1,8 @@
 package dio.budgeting.infrastructure.http;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +10,9 @@ import dio.budgeting.application.ListBudgetLimitUseCase;
 import dio.budgeting.application.ListGoalUseCase;
 import dio.budgeting.application.ListTransactionsByUserUseCase;
 import dio.budgeting.providers.AuthenticatedUserProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/dashboard")
@@ -15,5 +21,26 @@ public class DashboardController {
     private final ListGoalUseCase listGoalUseCase;
     private final ListBudgetLimitUseCase listBudgetLimitUseCase;
     private final AuthenticatedUserProvider authenticatedUserProvider;
+    private final ChatClient chatClient;
+
+    public DashboardController(
+            ListTransactionsByUserUseCase listTransactionsByUserUseCase,
+            ListGoalUseCase listGoalUseCase,
+            ListBudgetLimitUseCase listBudgetLimitUseCase,
+            AuthenticatedUserProvider authenticatedUserProvider,
+            @Value("classpath:/prompts/system-message.st") Resource systemPrompt,
+            ChatClient.Builder chatClientBuilder) {
+        this.listTransactionsByUserUseCase = listTransactionsByUserUseCase;
+        this.listGoalUseCase = listGoalUseCase;
+        this.listBudgetLimitUseCase = listBudgetLimitUseCase;
+        this.authenticatedUserProvider = authenticatedUserProvider;
+        this.chatClient = chatClientBuilder.defaultSystem(systemPrompt)
+                .defaultTools(listTransactionsByUserUseCase, listGoalUseCase, listBudgetLimitUseCase).build();
+    }
+
+    @GetMapping
+    public String get(@RequestParam String param) {
+        return new String();
+    }
     
 }
